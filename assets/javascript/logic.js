5692d24012a1d6671646d775575db553201a6ce9
Jay <paxformatus@gmail.com>
2018-07-26 15:43:31 -0400

    Portfolio commit

---
+++ assets/javascript/logic.js
@@ -0,0 +1,44 @@
+var beatTheOdds = function() {
+    $('#screen').html("<div class='col-md-12 beat'>" +
+    "<center>" +
+        "<img src='assets/images/beattheodds.gif' id='beatgif'>" +
+        "</center>" +
+    "</div>" +
+    "<div class='col-md-12 beat'>" +
+        "<div class='card-body portcard'>" +
+        "<h5 class='card-title'>Beat The Odds</h5>" +
+        "<p class='card-text'>Beat the Odds was a project designed to take historic NFL games, condense them down into a two minute window, and show the game graphically. The user is then able to place a bet on the teams displayed, with a payout decided by score differential.</p>" +
+        "<a href='https://jaykrauss.github.io/BeatTheOdds/' class='btn btn-primary'>Check it out!</a>" +
+        " </div>" +
+    "</div>")
+}
+
+var simpsonsTrivia = function() {
+    $('#screen').html("<div class='col-md-12 simpsons'>" +
+    "<center>" +
+    "<img src='assets/images/simpsonstrivia.gif' id='simpsonsgif'>" +
+    "</center>" +
+  "</div>" +
+  "<div class='col-md-12 simpsons'>" +
+    "<div class='card-body portcard'>" +
+      "<h5 class='card-title'>Simpsons Trivia</h5>" +
+      "<p class='card-text'>For the Simpsons fan that has been everywhere else, we have a Simpsons trivia game. Users progress through 20 questions of varying difficulty, then are given a grade (represented by 8 classic characters) based on their score.</p>" +
+      "<a href='https://jaykrauss.github.io/TriviaGame/' class='btn btn-primary'>Check it out!</a>" +
+   "</div>" +
+  "</div>")
+}
+
+var gifSplosion = function(){
+    $('#screen').html("<div class='col-md-12 gify'>" +
+    "<center>" +
+    "<img src='assets/images/gify.png' id='gify'>" +
+    "</center>" +
+  "</div>" +
+  "<div class='col-md-12 gify'>" +
+    "<div class='card-body portcard'>" +
+      "<h5 class='card-title'>GIFSPLOSION</h5>" +
+      "<p class='card-text'>A simple gify-API call to generate gif images for a given show. Users are able to find their favorite shows in gif form!</p>" +
+      "<a href='https://jaykrauss.github.io/APIgify/' class='btn btn-primary'>Check it out!</a>" +
+    "</div>" +
+  "</div>")
+}
