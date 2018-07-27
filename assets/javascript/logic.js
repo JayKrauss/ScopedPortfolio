9b92c91f0b1e98567af26d7d3cb82c6c0827e30e
Jay Krauss <paxformatus@gmail.com>
2018-07-26 20:22:50 -0400

    Resume first commit

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,67 +1,131 @@
 var beatTheOdds = function() {
-    $('#screen').html("<div class='col-md-12 beat'>" +
-    "<center>" +
-        "<img src='assets/images/beattheodds.gif' id='beatgif'>" +
-        "</center>" +
-    "</div>" +
-    "<div class='col-md-12 beat'>" +
-        "<div class='card-body portcard'>" +
-        "<h5 class='card-title'>Beat The Odds</h5>" +
-        "<p class='card-text'>Beat the Odds was a project designed to take historic NFL games, condense them down into a two minute window, and show the game graphically. The user is then able to place a bet on the teams displayed, with a payout decided by score differential.</p>" +
-        "<a href='https://jaykrauss.github.io/BeatTheOdds/' class='btn btn-primary'>Check it out!</a>" +
-        " </div>" +
-    "</div>");
+    $('#screen').html(`
+    <div class='col-md-12 beat'>
+    <center>
+        <img src='assets/images/beattheodds.gif' id='beatgif'>
+    </center>
+    </div>
+    <div class='col-md-12 beat'>
+        <div class='card-body portcard'>
+        <h5 class='card-title'>Beat The Odds</h5>
+        <p class='card-text'>Beat the Odds was a project designed to take historic NFL games, condense them down into a two minute window, and show the game graphically. The user is then able to place a bet on the teams displayed, with a payout decided by score differential.</p>
+        <a href='https://jaykrauss.github.io/BeatTheOdds/' class='btn btn-primary'>Check it out!</a>
+        </div>
+    </div>
+    `);
 }
 
 var simpsonsTrivia = function() {
-    $('#screen').html("<div class='col-md-12 simpsons'>" +
-    "<center>" +
-    "<img src='assets/images/simpsonstrivia.gif' id='simpsonsgif'>" +
-    "</center>" +
-  "</div>" +
-  "<div class='col-md-12 simpsons'>" +
-    "<div class='card-body portcard'>" +
-      "<h5 class='card-title'>Simpsons Trivia</h5>" +
-      "<p class='card-text'>For the Simpsons fan that has been everywhere else, we have a Simpsons trivia game. Users progress through 20 questions of varying difficulty, then are given a grade (represented by 8 classic characters) based on their score.</p>" +
-      "<a href='https://jaykrauss.github.io/TriviaGame/' class='btn btn-primary'>Check it out!</a>" +
-   "</div>" +
-  "</div>");
+    $('#screen').html(`
+    <div class='col-md-12 simpsons'>
+    <center>
+    <img src='assets/images/simpsonstrivia.gif' id='simpsonsgif'>
+    </center>
+  </div>
+  <div class='col-md-12 simpsons'>
+    <div class='card-body portcard'>
+      <h5 class='card-title'>Simpsons Trivia</h5>
+      <p class='card-text'>For the Simpsons fan that has been everywhere else, we have a Simpsons trivia game. Users progress through 20 questions of varying difficulty, then are given a grade (represented by 8 classic characters) based on their score.</p>
+      <a href='https://jaykrauss.github.io/TriviaGame/' class='btn btn-primary'>Check it out!</a>
+   </div>
+  </div>
+  `);
 }
 
 var gifSplosion = function(){
-    $('#screen').html("<div class='col-md-12 gify'>" +
-    "<center>" +
-    "<img src='assets/images/gify.png' id='gify'>" +
-    "</center>" +
-  "</div>" +
-  "<div class='col-md-12 gify'>" +
-    "<div class='card-body portcard'>" +
-      "<h5 class='card-title'>GIFSPLOSION</h5>" +
-      "<p class='card-text'>A simple gify-API call to generate gif images for a given show. Users are able to find their favorite shows in gif form!</p>" +
-      "<a href='https://jaykrauss.github.io/APIgify/' class='btn btn-primary'>Check it out!</a>" +
-    "</div>" +
-  "</div>");
+    $('#screen').html(`
+    <div class='col-md-12 gify'>
+    <center>
+    <img src='assets/images/gify.png' id='gify'>
+    </center>
+  </div>
+  <div class='col-md-12 gify'>
+    <div class='card-body portcard'>
+      <h5 class='card-title'>GIFSPLOSION</h5>
+      <p class='card-text'>A simple gify-API call to generate gif images for a given show. Users are able to find their favorite shows in gif form!</p>
+      <a href='https://jaykrauss.github.io/APIgify/' class='btn btn-primary'>Check it out!</a>
+    </div>
+  </div>
+  `);
 }
 
 var home = function(){
-    $('#screen').html("");
+    $('#screen').html(`
+    
+    `);
 }
 
 var basicInfo = function(){
-  $('#screen').html("<div class='card-body'>" +
-    "<center>" +
-    "<h5 class='card-title'>Basic Information</h5>" +
-    "</center>" +
-    "<p class='card-text'>A student and lover of learning, I have been transitioning my life drastically in recent months. From a tech manager to a mechanic and service manager, I have worn many hats, most recently the hat of web developer. Having been a part of a rigorous coding bootcamp at University of Richmond, I have learned many new things, and acquired many new skills.</p>" +
-    "<p class='card-text'>I have always been taught that in this life there are but a few definites, not including death or taxes: You are never done learning, if you aren't early you're late, and if you aren't the hardest working person in the room, you aren't working hard enough.</p>" +
-    "<p class='card-text'>As a budding developer, I am always looking for ways to improve and to learn, to build my growing list of skills.</p>"
-  );
+  $('#screen').html(`
+    <div class='card-body'>
+    <center>
+    <h5 class='card-title'>Basic Information</h5>
+    </center>
+    <p class='card-text'>A student and lover of learning, I have been transitioning my life drastically in recent months. From a tech manager to a mechanic and service manager, I have worn many hats, most recently the hat of web developer. Having been a part of a rigorous coding bootcamp at University of Richmond, I have learned many new things, and acquired many new skills.</p>
+    <p class='card-text'>I have always been taught that in this life there are but a few definites, not including death or taxes: You are never done learning, if you aren't early you're late, and if you aren't the hardest working person in the room, you aren't working hard enough.</p>
+    <p class='card-text'>As a budding developer, I am always looking for ways to improve and to learn, to build my growing list of skills.</p>
+  `);
 }
 
 var workExperience = function(){
-  $('#screen').html("");
+  $('#screen').html(`
+  <center>
+    <h2>Experience</h2>
+  </center>
+<br><br>
+<div class='row'>
+
+  <div class='card-body col-md-4'>
+  <h5 class='card-title'>Rivers West Design</h5>
+  <p class='card-text'>Founder, Developer<br>
+  Dates Employed: Sep 2017 – Present <br></p>
+  </div>
+
+  <div class='card-body col-md-4'>
+  <h5 class='card-title'>My.Com</h5>
+  </center>
+  <p class='card-text'>Community Contributor<br>
+  Dates Employed: Aug 2017 – Present <br></p>
+  </div>
+
+  <div class='card-body col-md-4'>
+  <h5 class='card-title'>AutoZone</h5>
+  </center>
+  <p class='card-text'>Commercial Sales Manager<br>
+  Dates Employed: Mar 2017 – Present <br></p>
+  </div>
+
+</div>
+<br><br>
+
+<div class='row'>
+  <div class='card-body col-md-4'>
+  <h5 class='card-title'>Firestone</h5>
+  </center>
+  <p class='card-text'>Service Manager<br>
+  Dates Employed: Dec 2014 – Mar 2017 <br></p>
+  </div>
+
+  <div class='card-body col-md-4'>
+  <h5 class='card-title'>MarketSource</h5>
+  </center>
+  <p class='card-text'>Sales Manager<br>
+  Dates Employed: Dec 2012 – Dec 2014 <br></p>
+  </div>
+
+  <div class='card-body col-md-4'>
+  <h5 class='card-title'>We R Wireless</h5>
+  </center>
+  <p class='card-text'>Store Manager<br>
+  Dates Employed: Dec 2011 – Jul 2012 <br></p>
+  </div>
+
+</div>
+  `);
 }
 
 var awards = function(){
-  $('#screen').html("");
+  $('#screen').html(`
+  
+  `);
 }
